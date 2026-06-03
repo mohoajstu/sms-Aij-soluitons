a632f4de70be295db118a98c92ac6ac0833969e0
Mohammed Abdul Jabbar <nuralhuda.co@gmail.com>
2026-06-03 09:30:41 -0400

    update quran report limit

--- src/views/ReportCard/Components/QuranReportUI.js
+++ src/views/ReportCard/Components/QuranReportUI.js
@@ -399,7 +399,7 @@ const CommentsSection = ({ formData, onFormDataChange }) => {
           onChange={handleInputChange}
           placeholder="Enter strengths and next steps for improvement in Quran Studies. You can use the AI button to generate personalized comments based on the student's grades..."
           rows={8}
-          maxLength={500}
+          maxLength={1000}
           formData={formData}
           onFormDataChange={onFormDataChange}
         />
